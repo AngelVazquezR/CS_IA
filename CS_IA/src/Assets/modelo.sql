911427d02c253cbdb18b0991b85ff09b6a351792
unnom <unnom@OMENBUGGY16>
2025-09-10 00:11:14 +0200

    Modelo de datos mejorado y librerias adicionales

--- CS_IA/src/Assets/modelo.sql
+++ CS_IA/src/Assets/modelo.sql
@@ -1,10 +1,21 @@
-CREATE TABLE `USUARIOS` (
-  `USUARIO` varchar(45) DEFAULT NULL,
-  `CONTRASEÑA` varchar(45) DEFAULT NULL,
-  UNIQUE KEY `idx_USUARIOS_USUARIO` (`USUARIO`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `ALUMNOS` (
+
+-- Volcando estructura de base de datos para csia
+DROP DATABASE IF EXISTS `csia`;
+CREATE DATABASE IF NOT EXISTS `csia` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `csia`;
+
+-- Volcando estructura para tabla csia.alumnos
+DROP TABLE IF EXISTS `alumnos`;
+CREATE TABLE IF NOT EXISTS `alumnos` (
   `ID` varchar(6) DEFAULT NULL,
   `NOMBRE` varchar(45) DEFAULT NULL,
   `APELLIDO` varchar(45) DEFAULT NULL,
@@ -12,7 +23,11 @@ CREATE TABLE `ALUMNOS` (
   `PROFESOR` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `PROFESORES` (
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla csia.profesores
+DROP TABLE IF EXISTS `profesores`;
+CREATE TABLE IF NOT EXISTS `profesores` (
   `ID` varchar(6) NOT NULL,
   `NOMBRE` varchar(45) DEFAULT NULL,
   `APELLIDOS` varchar(45) DEFAULT NULL,
@@ -21,3 +36,21 @@ CREATE TABLE `PROFESORES` (
   `FBAJA` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla csia.usuarios
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `USUARIO` varchar(45) DEFAULT NULL,
+  `CONTRASEÑA` varchar(45) DEFAULT NULL,
+  UNIQUE KEY `idx_USUARIOS_USUARIO` (`USUARIO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
